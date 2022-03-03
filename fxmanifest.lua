a55e8f5a5d7e34fd34db7727ce00920346892ee3
code not my father <84304475+idontknowtoobrother@users.noreply.github.com>
2022-03-03 19:07:52 +0700

    initial: back-end

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'celerean'
+game 'gta5'
+
+client_script {
+    'Secure.lua',
+    'Client.lua'
+}
+
+server_script 'Server.lua'
+
+ui_page 'Interface/ui.html'
+
+files {
+    'Interface/control.js',
+    'Interface/style.css',
+    'Interface/ui.html',
+    'Interface/*.png',
+    'Interface/*.gif',
+    'Interface/*.jpg'
+}
