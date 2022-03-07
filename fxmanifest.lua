0c82884a64fa02a557073999b9dcf10107ef894a
code not my father <84304475+idontknowtoobrother@users.noreply.github.com>
2022-03-07 21:42:35 +0700

    fix bot unknow channel

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,7 +11,7 @@ game 'gta5'
 
 author 'Hex ( keng#0110 )'
 description 'Ez Serial Code Topup with discord bot'
-version '1.0 Try Harder Release'
+version '1.1 update bot error "Unknow Channel"'
 
 client_scripts {
     'Settings.lua',
